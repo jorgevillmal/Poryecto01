8a05b3a8ba582c937b9d4bb87011e4b575d3b87c
Jorge Villarreal <jorgevillmal@ciencias.unam.mx>
2020-11-14 06:34:41 -0600

    Actualizando

---
+++ cocinando/HacerBurrito.java
@@ -0,0 +1,15 @@
+public class HacerBurrito extends Cocina{
+  System.out.println("Sacando la tortilla...");
+  System.out.println("Colocando frijoles en la tortilla...");
+  System.out.println("Colocando vegetales en la tortilla...");
+  System.out.println("Colocando carne en la tortilla...");
+  System.out.println("Colocandola en el comal");
+  System.out.println("");
+  System.out.println("");
+  System.out.println("");
+}
+
+
+El primer módulo de funcionamiento incluye espátula, plancha, panes, lechuga, jitomates, carne, etc. Con este módulo es capaz de cocinar hamburguesas.
+El segundo módulo de funcionamiento incluye horno, masa de pizza, salsa de tomate, queso, pepperoni, pimientos, champiñones, etc. Con este módulo es capaz de cocinar pizzas.
+El tercer módulo de funcionamiento incluye comal, tortillas, frijoles, carne, vegetales, etc. Con este módulo es capaz de cocinar burritos.
